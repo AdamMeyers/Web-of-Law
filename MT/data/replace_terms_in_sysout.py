50c819ac7c95aac368f84118fe871c44421ce77f
Esteban Galvis <eg2680@proteus7.cs.nyu.edu>
2017-07-26 16:26:36 -0400

    RESULTS

--- MT/data/replace_terms_in_sysout.py
+++ MT/data/replace_terms_in_sysout.py
@@ -7,7 +7,7 @@ import sys
 import re
 import codecs
 
-new_file = 'test.europarl.es.10000.moses.lc.replaced.txt'
+new_file = 'test.test.europarl.es.10000.moses.lc.replaced.txt'
 
 base_moses_sentences = []
 # gather all lines in a list
@@ -43,8 +43,7 @@ human_translated_terms = {}
 with codecs.open(sys.argv[3],'r',encoding='utf8') as f:
     for line in f:
         new_line = line.strip()
-        tab_line = new_line.split('###')
-        print tab_line
+        tab_line = new_line.split('#%#')
         eng_term    = tab_line[0]
         sp_term     = tab_line[1]
         human_translated_terms[eng_term] = sp_term
@@ -53,19 +52,25 @@ with codecs.open(sys.argv[3],'r',encoding='utf8') as f:
 ## now we have a dictionary with new term replacements
 ## get original file and replace
 with codecs.open(new_file,'w',encoding='utf8') as final:
-    counter = 1
+    counter =1
     for sent in base_moses_sentences:
         newsent = sent
-        if counter in lines_with_terms_and_positions:
-            tpl = lines_with_terms_and_positions[counter]
+        if unicode(str(counter),"utf-8") in lines_with_terms_and_positions:
+            tpl = lines_with_terms_and_positions[unicode(str(counter),"utf-8")]
             eng_term  = tpl[0]
             pos_start = tpl[1]
             pos_end   = tpl[2]
-            if human_translated_terms[eng_term]:
+            if eng_term in human_translated_terms:
                 # replace with new spanish term
-                newsent = newsent[0:pos_start] + human_translated_terms[eng_term] + newsent[pos_end+1:] 
+                print counter
+                #There is a bug that doesnt find an index eng_word, this is because it is june 1998 and 
+                #the translated text by moses outputs it 1998 aldana june
+                if eng_term in newsent:
+                    new_index_start  = newsent.index(eng_term) 
+                    new_index_end    = new_index_start + len(eng_term)
+                    newsent = newsent[0:int(new_index_start)] + ' ' + human_translated_terms[eng_term] + ' '+ newsent[int(new_index_end)+1:]
         final.write(newsent)
-        print newsent 
+        final.write('\n')
         counter = counter + 1
 final.close()
 
